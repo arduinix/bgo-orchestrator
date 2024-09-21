b2561cd3aab80e496100c4557078e29758f4a756
arduinix <arduinix88@gmail.com>
2024-09-21 17:40:36 -0600

    more components

---
+++ bgo-web-ui/src/pages/my-olympics/MyOlympics.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function MyOlympics() {
+  return (
+    <Box p={2} pt={4} position="relative">
+      Placeholder for MyOlympics.
+    </Box>
+  );
+}
