81a9aa536aa02f2aab77566453330bb19ea5db02
arduinix <arduinix88@gmail.com>
2024-09-25 14:17:59 -0600

    add a homepage hero for bgo

--- bgo-web-ui/src/pages/home/Home.tsx
+++ bgo-web-ui/src/pages/home/Home.tsx
@@ -1,12 +1,10 @@
 import { Box } from "@chakra-ui/react";
 import logo_no_info from "../../assets/image/logo_no_info.png";
+import HomepageBgoHero from "../../components/home-page-bgo-hero/HomepageBgoHero";
 
 export default function Home() {
   return <>
-    <Box>
-      <img src={logo_no_info} alt="logo" />
-      <h1>Welcome to the BGO Web UI</h1>
-    </Box>
+  <HomepageBgoHero />
   
   
   </>;
