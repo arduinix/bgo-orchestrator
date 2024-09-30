b71b3c898b7a62606f946b97569e4f40126849c2
arduinix <arduinix88@gmail.com>
2024-09-29 19:06:05 -0600

    make it a little better

--- bgo-web-ui/src/pages/event-editor/EventEditor.tsx
+++ bgo-web-ui/src/pages/event-editor/EventEditor.tsx
@@ -6,36 +6,37 @@ import { RxDashboard } from 'react-icons/rx'
 import { FaUserSecret } from 'react-icons/fa6'
 export default function EventEditor() {
   const { id } = useParams()
+  const route = `/olympics/${id}`
 
   const linkItems: Array<LinkItemProps> = [
     {
       name: 'Registration',
       icon: FaUserSecret,
-      href: `/olympics/${id}/registration`,
+      href: `${route}/registration`,
       tooltip: 'Register players and manage player information.',
     },
     {
       name: 'Games & Tables',
       icon: GiPerspectiveDiceSixFacesOne,
-      href: `/olympics/${id}/games`,
+      href: `${route}/games`,
       tooltip: 'Set up games and player tables.',
     },
     {
       name: 'Rounds',
       icon: GiTimeBomb,
-      href: `/olympics/${id}/rounds`,
+      href: `${route}/rounds`,
       tooltip: 'Start and stop rounds edit scores',
     },
     {
       name: 'Event Resources',
       icon: RiQrCodeFill,
-      href: `/olympics/${id}/resources`,
+      href: `${route}/resources`,
       tooltip: 'Manage event resources and printable materials.',
     },
     {
       name: 'Dashboards',
       icon: RxDashboard,
-      href: `/olympics/${id}/dashboards`,
+      href: `${route}/dashboards`,
       tooltip: 'View event statistics and analytics.',
     },
   ]
