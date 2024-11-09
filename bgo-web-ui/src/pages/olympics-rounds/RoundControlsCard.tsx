5a0880acd07dbea03998e17542da4d4fe0005520
arduinix <arduinix88@gmail.com>
2024-11-09 10:08:25 -0700

    work on round controls

--- bgo-web-ui/src/pages/olympics-rounds/RoundControlsCard.tsx
+++ bgo-web-ui/src/pages/olympics-rounds/RoundControlsCard.tsx
@@ -1,33 +1,93 @@
 import {
   Card,
-  CardHeader,
   CardBody,
-  CardFooter,
   Text,
   Box,
   Flex,
   Select,
-  FormLabel,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  IconButton,
 } from '@chakra-ui/react'
 import games from '@data/games.json'
+import { GiChoice } from 'react-icons/gi'
+import NumberPicker from '@/components/number-picker/NumberPicker'
+import { FaPlay, FaStop, FaChevronDown } from 'react-icons/fa'
 
 export default function RoundControlsCard() {
   const categories: GameCategory[] = games.catagories
   return (
     <Card variant={'filled'} size={'sm'}>
-      <Box background={'white'} m={2}>
+      <Box background={'white'} m={1}>
         <CardBody>
-          <Flex>
-            <Box display={'flex'}>
-              <Text fontWeight={'bold'}>Select Category</Text>
-              <Select>
-                {categories.map(({ id, name }) => (
-                  <option key={id} value={name}>
-                    {name}
-                  </option>
-                ))}
-              </Select>
-            </Box>
+          <Flex alignItems={'center'} m={-1} gap={2}>
+            <Flex
+              borderWidth={'1px'}
+              borderColor={'gray.100'}
+              alignItems={'center'}
+              gap={2}
+            >
+              <Box display={'flex'} alignItems={'center'} m={1} mr={2}>
+                <Text fontWeight={'bold'} sx={{ whiteSpace: 'nowrap' }} mr={2}>
+                  Select Category:
+                </Text>
+                <Select justifySelf={'flex-end'}>
+                  {categories.map(({ id, name }) => (
+                    <option key={id} value={name}>
+                      {name}
+                    </option>
+                  ))}
+                </Select>
+              </Box>
+              <Button size={'md'} rightIcon={<GiChoice />}>
+                Auto Pick
+              </Button>
+            </Flex>
+            <Flex
+              borderWidth={'1px'}
+              borderColor={'gray.100'}
+              alignItems={'center'}
+              gap={2}
+            >
+              <Box display={'flex'} alignItems={'center'} m={1} mr={2}>
+                <Text fontWeight={'bold'} sx={{ whiteSpace: 'nowrap' }} mr={2}>
+                  Tables:
+                </Text>
+                <NumberPicker value={1} onChange={() => {}} />
+              </Box>
+            </Flex>
+            <Menu>
+              <MenuButton as={Button} rightIcon={<FaChevronDown />}>
+                Match Actions
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Auto Create All</MenuItem>
+                <MenuItem>Assign All Benched Players</MenuItem>
+                <MenuItem>Create New Match</MenuItem>
+                <MenuItem>Delete Select Match</MenuItem>
+              </MenuList>
+            </Menu>
+
+            <IconButton
+              colorScheme='red'
+              aria-label='stop'
+              icon={<FaStop />}
+              variant={'outline'}
+            />
+            <IconButton
+              colorScheme='green'
+              aria-label='start'
+              icon={<FaPlay />}
+              variant={'outline'}
+              // set up so the if playing the variant is solid
+            />
           </Flex>
         </CardBody>
       </Box>
